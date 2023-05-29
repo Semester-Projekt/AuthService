ba4366e322cbbfc908c69ec492a535cc3a1f91f6
danieldawsonjensen <dani540c@gmail.com>
2023-05-29 17:11:33 +0200

    auth login fix?

--- auth-service/Controllers/AuthController.cs
+++ auth-service/Controllers/AuthController.cs
@@ -30,6 +30,9 @@ public class AuthController : ControllerBase
         var _ipaddr = ips.First().MapToIPv4().ToString();
         _logger.LogInformation(1, $"Auth service responding from {_ipaddr}");
 
+        _logger.LogInformation($"Connecting to rabbitMQ on {_config["rabbithostname"]}");
+
+        _logger.LogInformation($"USER_SERVICE_URL: {_config["USER_SERVICE_URL"]}");
     }
 
     private string GenerateJwtToken(string username)
@@ -64,8 +67,12 @@ public class AuthController : ControllerBase
 
         using (HttpClient client = new HttpClient())
         {
+            _logger.LogInformation("HTTPClient intialized");
+
             string userServiceUrl = Environment.GetEnvironmentVariable("USER_SERVICE_URL"); // retreives URL to UserService from docker-compose.yml file
             string getUserEndpoint = "/user/getUser/" + userId;
+
+            _logger.LogInformation($"AuthService - {userServiceUrl + getUserEndpoint}");
 
             HttpResponseMessage response = await client.GetAsync(userServiceUrl + getUserEndpoint); // calls the UserService endpoint
 
